57807413693dcc44fb9045aec77fd2d771508feb
Lucie Aimar <lucie.aimar13@gmail.com>
2026-07-08 22:09:03 +0200

    home view ok; edit index.php;  delete test files

---
+++ app/Views/home.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Touche pas au klaxon</title>
+</head>
+
+<body>
+
+    <h1>Trajets disponibles</h1>
+
+    <table border="1" cellpadding="8">
+        <thead>
+            <tr>
+                <th>Départ</th>
+                <th>Date de départ</th>
+                <th>Arrivée</th>
+                <th>Date d'arrivée</th>
+                <th>Places disponibles</th>
+            </tr>
+        </thead>
+
+        <tbody>
+
+        <?php foreach ($trajets as $trajet): ?>
+
+            <tr>
+                <td><?= htmlspecialchars($trajet['ville_depart']) ?></td>
+
+                <td><?= htmlspecialchars($trajet['date_heure_depart']) ?></td>
+
+                <td><?= htmlspecialchars($trajet['ville_arrivee']) ?></td>
+
+                <td><?= htmlspecialchars($trajet['date_heure_arrivee']) ?></td>
+
+                <td><?= htmlspecialchars($trajet['places_disponibles']) ?></td>
+            </tr>
+
+        <?php endforeach; ?>
+
+        </tbody>
+    </table>
+
+</body>
+
+</html>
